e2d939535eff32261d78835f4bfb90a0d8f1b911
danieldaga <daniel@arhrestauracion.com>
2022-10-24 21:23:44 +0200

    Ex2

--- ex1/main.js
+++ ex1/main.js
@@ -1,6 +1,7 @@
 
 for (let i = 1; i <= 50; i++) {
     
+    
     if (i % 10 === 0 || i % 15 === 0) {
         console.log('Donkey!');
     }
@@ -8,11 +9,13 @@ for (let i = 1; i <= 50; i++) {
     if (i % 5 === 0) {
         continue
     }
+
+    if (!i % 2 === 0 && (!i - 1) % 10 === 0 ) {
+        console.log('Donkey!');
+    }
+
     console.log(i);
     
     
 
-    if (!i % 2 === 0 && !i - 1 % 10 === 0 ) {
-        console.log('Donkey!');
-    }
 }
---
+++ ex2/main.js
@@ -0,0 +1,27 @@
+let n = prompt('Hasta que numero quieres que cuente?')
+for (let i = 1; i <= n; i++) {
+    console.log(i);
+}
+
+let N = prompt('Hasta que numero quieres que cuente?')
+for (let i = 1; i <= N; i+=2) {
+    
+    console.log(i);
+}
+
+let F = prompt('Desde que numero quieres que cuente alreves?')
+for (let i = F; i <= F; i--) {
+    if (i === 0) {
+        break;
+    }
+    console.log(i);
+    
+}
+
+for (i = 0; i <= 10; i++) {
+    console.log("9 x", i, "=", 9 * i);
+}
+
+
+
+
