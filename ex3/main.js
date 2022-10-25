be099cb64563ec867b23b73511088a9c89cecc12
danieldaga <daniel@arhrestauracion.com>
2022-10-25 16:46:10 +0200

    ex3 finished

--- ex3/main.js
+++ ex3/main.js
@@ -3,9 +3,9 @@ let month = prompt('Escribe del 1 al 12 el mes que quieres saber los días que t
 if (month >=12 || month < 1) {
     alert('Introduce un mes valido')
 } else{
-    if (month % 2 === 0) {
-        alert('este mes tiene 30 días')
-    } else{
+    if (month === [1,3,5,7,8,10,12]) {
         alert('este mes tiene 31 días')
+    } else{
+        alert('este mes tiene 30 días')
     }
 }
